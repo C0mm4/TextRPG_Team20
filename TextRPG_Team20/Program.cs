797c853749bb6a4464db138a8af1144f66e06a09
C0mm4 <comonhuman@gmail.com>
2025-07-14 17:44:27 +0900

    Chore : KHU_폴더링, 게임 콘솔 사이즈 설정

--- TextRPG_Team20/Program.cs
+++ TextRPG_Team20/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.SetWindowSize(160, 50);   // 가로 80, 세로 30
+            Console.SetBufferSize(160, 50);   // 버퍼도 동일하게 설정
         }
     }
 }
