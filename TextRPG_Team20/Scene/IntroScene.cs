4f1564a93fba3ee591fbd23e57f887eadb032972
C0mm4 <comonhuman@gmail.com>
2025-07-14 20:09:37 +0900

    feat : KHU_IScene 구조 변경 및 하위 Scene 추가

---
+++ TextRPG_Team20/Scene/IntroScene.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team20.Scene
+{
+    internal class IntroScene : IScene
+    {
+        public bool Action(int input)
+        {
+            switch (input) 
+            {
+                case 1:
+                    Console.WriteLine("Welcome");
+                    // Add Player Instance Initialize
+
+                    Game.Instance.SceneChange(Game.SceneState.Lobby);
+                    return false;
+                case 2:
+                    Console.WriteLine("Okay...");
+                    return true;
+                default:
+                    Console.WriteLine("Input Error!");
+                    return true;
+
+            }
+
+        }
+
+        public void PrintScene()
+        {
+            Console.WriteLine("Welcome to this game");
+            Console.Write("Please input your charachor name >> ");
+            string? s = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"{s} is your name. right?");
+            Console.WriteLine();
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+        }
+    }
+}
