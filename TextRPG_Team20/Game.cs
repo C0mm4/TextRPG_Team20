f356fe51cc1459a95ac2c18691c9385222b32275
C0mm4 <comonhuman@gmail.com>
2025-07-14 16:37:43 +0900

    Chore : KHU_기본 게임 인터페이스 추가

---
+++ TextRPG_Team20/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team20
+{
+    public class Game
+    {
+        private Game _instance;
+        public Game Instance
+        {
+            get 
+            { 
+                if(_instance == null)
+                {
+                    _instance = new Game();
+                } return _instance;
+            }
+        }
+
+        public struct SceneState
+        {
+
+        }
+
+        private SceneState _state;
+
+        public Game()
+        {
+
+        }
+
+        public void SceneChange(SceneState state)
+        {
+
+        }
+
+        public void PopScene()
+        {
+
+        }
+
+        public void SaveGame()
+        {
+
+        }
+
+        public void LoadGame() 
+        {
+
+        }
+    }
+}
---
+++ TextRPG_Team20/Item/IItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team20
+{
+    internal class ItemData
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string FlavorText { get; set; }
+        public int Gold { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int HP { get; set; }
+
+        public int Type { get; set; }
+
+    }
+
+    internal class Item : IComponent, IComparable<Item>
+    {
+        ItemData data;
+
+        public int CompareTo(Item? other)
+        {
+            if (other != null)
+            {
+                return data.Id.CompareTo(other.data.Id);
+            }
+            return 0;
+        }
+
+        public void PrintData()
+        {
+
+        }
+
+        public void PrintInField()
+        {
+
+        }
+    }
+
+    internal class ItemWrapper()
+    {
+        public required List<ItemData> Items { get; set; }
+    }
+}
