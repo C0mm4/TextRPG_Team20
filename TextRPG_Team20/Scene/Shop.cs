6ddf3325bd477eb3785049e3dd5f156601035f51
GinMyo <124099502+GinMyo@users.noreply.github.com>
2025-07-14 19:37:01 +0900

    [Feat]KMK: 상점 클래스 IScene 인터페이스 참조

---
+++ TextRPG_Team20/Scene/Shop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG_Team20.Scene;
+
+namespace TextRPG_Team20
+{
+    internal class Shop : IScene
+    {
+        public void PrintScene()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("상점");
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+
+                Console.WriteLine("[보유 골드]");
+               // Console.WriteLine(playerStatus.gold + "G\n");
+
+                Console.WriteLine("[아이템 목록]");
+                // 아이템 목록 연결
+               // system_Shop.ShopList();
+
+                Console.WriteLine("");
+
+                Console.WriteLine("1. 아이템 구매");
+                Console.WriteLine("0. 나가기");
+
+
+              }
+         }
+  
+    }
+}
