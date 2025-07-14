9ddc1cb5b2e5123f26ede22a88be0fc054eec6c1
C0mm4 <comonhuman@gmail.com>
2025-07-14 19:11:49 +0900

    feat : KHU_게임 씬 컨트롤

--- TextRPG_Team20/Game.cs
+++ TextRPG_Team20/Game.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG_Team20.Scene;
 
 namespace TextRPG_Team20
 {
@@ -20,12 +22,14 @@ namespace TextRPG_Team20
             }
         }
 
-        public struct SceneState
+        public enum SceneState
         {
-
+            Title, Lobby, DungeonSelect, InField, Battle, Result, Shop, Inventory, EquipControl, Status, SkillList, UseItem, 
         }
 
         private SceneState _state;
+        private Stack<IScene> sceneStack;
+        private IScene currentScene;
 
         public Game()
         {
@@ -34,12 +38,55 @@ namespace TextRPG_Team20
 
         public void SceneChange(SceneState state)
         {
+            IScene? newScene = null;
+            switch (state)
+            {
+                case SceneState.Title:
+                    break;
+                case SceneState.Lobby:
+                    break;
+                case SceneState.DungeonSelect:
+                    break;
+                case SceneState.InField:
+                    break;
+                case SceneState.Battle:
+                    break;
+                case SceneState.Result:
+                    break;
+                case SceneState.Shop:
+                    break;
+                case SceneState.Inventory:
+                    break;
+                case SceneState.EquipControl:
+                    break;
+                case SceneState.Status:
+                    break;
+                case SceneState.SkillList:
+                    break;
+                case SceneState.UseItem:
+                    break;
 
+            }
+            if (newScene != null) 
+            {
+                if(currentScene.GetType().Name != "IntroScene")
+                {
+                    sceneStack.Push(currentScene);
+                }
+                currentScene = newScene;
+            }
         }
 
         public void PopScene()
         {
-
+            if(sceneStack.Count > 0)
+            {
+                currentScene = sceneStack.Pop();
+            }
+            else
+            {
+                currentScene = null;
+            }
         }
 
         public void SaveGame()
