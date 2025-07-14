1cd177a19a333108f7e3724b080cb3b9117cb30f
GinMyo <124099502+GinMyo@users.noreply.github.com>
2025-07-14 19:59:56 +0900

    [Feat]KMK: Buy, Sell 메서드 추가
    자세한 기능구현은 아이템이 완성되면 추가

--- TextRPG_Team20/Scene/Shop.cs
+++ TextRPG_Team20/Scene/Shop.cs
@@ -9,29 +9,39 @@ namespace TextRPG_Team20
 {
     internal class Shop : IScene
     {
+
+        //씬 출력 메서드
         public void PrintScene()
         {
 
             while (true)
             {
                 Console.WriteLine("상점");
-                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
-
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+                Console.WriteLine("");
                 Console.WriteLine("[보유 골드]");
-               // Console.WriteLine(playerStatus.gold + "G\n");
-
+                Console.WriteLine( "G");
+                Console.WriteLine("");
                 Console.WriteLine("[아이템 목록]");
                 // 아이템 목록 연결
-               // system_Shop.ShopList();
+                // system_Shop.ShopList();
 
                 Console.WriteLine("");
-
                 Console.WriteLine("1. 아이템 구매");
                 Console.WriteLine("0. 나가기");
+            }
 
+            bool BuyItem()
+            {
+            return true;
+            }
+            bool SellItem()
+            {
+            return true;
+            }
 
-              }
-         }
+        }
+
   
     }
 }
