ab9f3ace565e5a6f5353f713edf4bc917ac5a698
GinMyo <124099502+GinMyo@users.noreply.github.com>
2025-07-15 11:05:28 +0900

    [Feat]KMK: action 메서드 연결

--- TextRPG_Team20/Scene/ShopScene.cs
+++ TextRPG_Team20/Scene/ShopScene.cs
@@ -3,41 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TextRPG_Team20.Scene;
 
-namespace TextRPG_Team20
+namespace TextRPG_Team20.Scene
 {
     internal class ShopScene : IScene
     {
+        private int BuyScene = 0;
+
+        public bool Action(int input)
+        {
+            if (BuyScene == 0)
+            {
+                switch (input)
+                {
+                    case 0:
+                        Game.Instance.PopScene();
+                        return false;
+                    case 1:
+                        BuyScene = 1;
+                        return false;
+                    default:
+                        Console.WriteLine("Input Error!");
+                        return true;
+                }
+            }
+            else
+            {
+                switch (input)
+                {
+                    case 0:
+                        Game.Instance.PopScene();
+                        BuyScene = 0;
+                        return false;
+                    default:
+                        Console.WriteLine("Input Error!");
+                        return true;
+                }
+            }
+        }
+
 
         //씬 출력 메서드
         public void PrintScene()
         {
-
-            while (true)
+            if(BuyScene == 0) 
             {
-                Console.WriteLine("상점");
+                Console.WriteLine($"{AnsiColor.Yellow}상점{AnsiColor.Reset}");
                 Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
                 Console.WriteLine("");
                 Console.WriteLine("[보유 골드]");
-                Console.WriteLine( "G");
+                Console.WriteLine($"{AnsiColor.Yellow} G{AnsiColor.Reset}");
                 Console.WriteLine("");
                 Console.WriteLine("[아이템 목록]");
                 // 아이템 목록 연결
-                // system_Shop.ShopList();
+
 
                 Console.WriteLine("");
                 Console.WriteLine("1. 아이템 구매");
                 Console.WriteLine("0. 나가기");
             }
+            else
+            {
+                Console.WriteLine($"{AnsiColor.Yellow}상점 - 구매{AnsiColor.Reset}");
+                Console.WriteLine("구매할 아이템의 번호를 입력해 주세요.");
+                Console.WriteLine("");
+                Console.WriteLine("[보유 골드]");
+                Console.WriteLine($"{AnsiColor.Yellow} G{AnsiColor.Reset}");
+                Console.WriteLine("");
+                Console.WriteLine("[아이템 목록]");
+                // 아이템 목록 연결
 
-            bool BuyItem()
-            {
-            return true;
-            }
-            bool SellItem()
-            {
-            return true;
+
+                Console.WriteLine("");
+                Console.WriteLine("0. 구매 종료");
             }
 
         }
