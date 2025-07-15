9433d18622b49b59291e47a55558c4cc2a863a4f
a2g2c3d3 <papa3520@naver.com>
2025-07-15 11:18:19 +0900

    feat_PJH 장착화면

---
+++ TextRPG_Team20/Scene/EquipControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team20.Scene
+{
+    internal class EquipScene : IScene
+    {
+        public bool Action(int input)
+        {
+            switch (input)
+            {
+                case 0:
+                    Game.Instance.SceneChange(Game.SceneState.Inventory);
+                    return false;
+                       
+                default:
+                    Console.WriteLine("Input Error!");
+                    return true;
+            }
+        }
+
+        public void PrintScene()
+        {
+            Console.Clear();
+            Console.WriteLine("인벤토리 - 장착관리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine();
+
+            //아이템 목록
+
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            Console.WriteLine(">>");
+
+
+
+        }
+    }
+}
