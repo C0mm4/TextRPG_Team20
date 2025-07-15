e8906e498963f7a9533759e2960f84b3df467f26
Kseongmin1107 <ksmsky1107@gmail.com>
2025-07-15 10:57:00 +0900

    Feat_DungeonSelectScene

--- TextRPG_Team20/Scene/DungeonSelectScene.cs
+++ TextRPG_Team20/Scene/DungeonSelectScene.cs
@@ -32,11 +32,23 @@ namespace TextRPG_Team20.Scene
         }
         public void PrintScene()
         {
-            Console.WriteLine($"{AnsiColor.Cyan}Dungeon Select{AnsiColor.Reset}");
-            Console.WriteLine("Choose a dungeon to enter:");
-            Console.WriteLine("Dungeon");
-            Console.WriteLine();
-            Console.WriteLine("0. Back to Lobby");
+            ConsoleUI.mainView.ClearBuffer();
+            ConsoleUI.logView.ClearBuffer();
+            ConsoleUI.info1View.ClearBuffer();
+            ConsoleUI.info2View.ClearBuffer();
+            ConsoleUI.inputView.ClearBuffer();
+
+            ConsoleUI.Instance.DrawTextInBox($"{AnsiColor.Cyan}Dungeon Select{AnsiColor.Reset}", ref ConsoleUI.mainView);
+            ConsoleUI.Instance.DrawTextInBox("Choose a dungeon to enter:", ref ConsoleUI.mainView);
+            ConsoleUI.Instance.DrawTextInBox("1. Dungeon", ref ConsoleUI.mainView);
+            ConsoleUI.Instance.DrawTextInBox("", ref ConsoleUI.mainView);
+            ConsoleUI.Instance.DrawTextInBox("0. Back to Lobby", ref ConsoleUI.mainView);
+
+            ConsoleUI.Instance.PrintView(ConsoleUI.mainView);
+            ConsoleUI.Instance.PrintView(ConsoleUI.logView);
+            ConsoleUI.Instance.PrintView(ConsoleUI.info1View);
+            ConsoleUI.Instance.PrintView(ConsoleUI.info2View);
+            ConsoleUI.Instance.PrintView(ConsoleUI.inputView);
         }
     }
     
