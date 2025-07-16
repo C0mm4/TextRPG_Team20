448874d0fff87ecb3d81af5517f5618f8065a432
GinMyo <124099502+GinMyo@users.noreply.github.com>
2025-07-16 15:18:56 +0900

    [Feat]KMK: 적 행동 및 예시 잡몹 2개 추가
    BasicEnemys.cs 에는 행동이 간단한 '잡몹'을 관리

--- TextRPG_Team20/Charactor/Enemy.cs
+++ TextRPG_Team20/Charactor/Enemy.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG_Team20.Scene;
 
 namespace TextRPG_Team20
 {
     internal class Enemy : Character
     {
 
-        public Enemy(string name, Status status) : base(name, "Enemy", 0, status)
+        public Enemy(string name, int gold,  Status status) : base(name, "Enemy", gold, status)
         {
 
         }
 
         public override void Action()
         {
-            ConsoleUI.Instance.DrawTextInBox($"{Name}이(가) 공격을 준비합니다.", ref ConsoleUI.logView);
-
+            ConsoleUI.Instance.DrawTextInBox($"{AnsiColor.Yellow}{status.Name}이(가) 공격을 준비합니다.{AnsiColor.Reset}", ref ConsoleUI.logView);
             ConsoleUI.Instance.PrintView(ref ConsoleUI.logView);         
+        }
+
+        public override void Attack(Character target)
+        {
+            int damage = status.TotalAtk;
+            int actualDamage = Math.Max(1, damage - target.status.TotalDef);
+            ConsoleUI.Instance.DrawTextInBox($"{AnsiColor.Red}{status.Name}{AnsiColor.Reset}이(가) {AnsiColor.Green}{target.status.Name}{AnsiColor.Reset}에게 일반 공격!", ref ConsoleUI.logView);
+            ConsoleUI.Instance.DrawTextInBox($"{AnsiColor.Red}{actualDamage}{AnsiColor.Reset}의 피해를 받았습니다.", ref ConsoleUI.logView);
+            target.DecreaseHp(damage);
+
+            ConsoleUI.Instance.PrintView(ref ConsoleUI.logView);
         }
     }
 }
